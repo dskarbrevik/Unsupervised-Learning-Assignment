a122ad904d09d628a3f4130e70222cdd613001bd
David Skarbrevik <skarbrevik@ischool.berkeley.edu>
2017-04-07 03:17:29 -0700

    updated (almost done)

--- W207_P3.py
+++ W207_P3.py
@@ -9,8 +9,10 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.mixture import GMM
 from matplotlib.colors import LogNorm
+from sklearn.grid_search import GridSearchCV
 
-os.chdir("/Users/Admiral/Desktop/W207_P3/")
+
+os.chdir("\\Users\\skarb\\Desktop\\Github\\W207_P3\\")
 
 feature_names = []
 with open('mushroom.map') as fmap:
@@ -42,7 +44,6 @@ test_data, test_labels = X[7000:], Y[7000:]
 
 # Check that the shapes look right.
 print (train_data.shape, test_data.shape)
-
 
 ###############################################################################
 # Problem 1
@@ -84,10 +85,11 @@ plt.show()
 # Problem 3
 ###############################################################################
 
-fig = plt.figure(figsize=(10, 30))
-for i in range(16):
+fig = plt.figure(figsize=(10, 5))
+count = 1
+for i in [1,16]:
 
-    model1 = KMeans(n_clusters=i+1)
+    model1 = KMeans(n_clusters=i)
     model1.fit(pca_dat)
     labels = model1.labels_
     centers = model1.cluster_centers_
@@ -103,24 +105,28 @@ for i in range(16):
         #print euclid_dist #why the hell is this always 1
         if euclid_dist > max_dist[predicted_cluster[j]]:
             max_dist[predicted_cluster[j]]=euclid_dist
-    sub_plot = fig.add_subplot(8,2,i+1)
+    sub_plot = fig.add_subplot(1,2,count)
     sub_plot.scatter(pca_dat[:,0], pca_dat[:,1], c = labels)
-    title_i = str(i+1) + " Clusters"
+    title_i = str(i) + " Clusters"
     for k in range(len(centers)):
         cir = plt.Circle(centers[k], radius = max_dist[k], color = 'g', fill = False)
         sub_plot.add_patch(cir)
+        sub_plot.scatter(model1.cluster_centers_[k][0], model1.cluster_centers_[k][1], color = "black")
     plt.title(title_i)
+    count+=1
 plt.show()
 
 ###############################################################################
 # Problem 4
 ###############################################################################
 
-model = GMM(n_components=2, covariance_type="full")
-model.fit(pca_dat)
-
-
-
+pos_labels = np.where(train_labels == 1)
+n_comp = 2
+pca_mod = PCA(n_components = n_comp)
+pca_dat = pca_mod.fit_transform(train_data)
+pos_pca = pca_dat[pos_labels]
+model = GMM(n_components=4, covariance_type="spherical")
+model.fit(pos_pca)
 
 # display predicted scores by the model as a contour plot
 x = np.linspace(-20., 30.)
@@ -128,62 +134,78 @@ y = np.linspace(-20., 40.)
 X, Y = np.meshgrid(x, y)
 XX = np.array([X.ravel(), Y.ravel()]).T
 Z = model.score_samples(XX)
-Z = Z.reshape(X.shape)
-type(XX)
+#Z = Z.reshape(X.shape)
 
-
-model.score_samples(XX)
-CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
-                 levels=np.logspace(0, 3, 10))
+test = np.empty([2500,])
+for i in range(len(Z[0])):
+    test[i] = Z[0][i]
+test = test.reshape(X.shape)
+test = -(test)
+fig = plt.figure(figsize=(10, 5))
+CS = plt.contour(X, Y, test, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10))
 CB = plt.colorbar(CS, shrink=0.8, extend='both')
-plt.scatter(pca_dat[:, 0], pca_dat[:, 1], .8)
-
+plt.scatter(pca_dat[:, 0], pca_dat[:, 1])
 plt.title('Negative log-likelihood predicted by a GMM')
 plt.axis('tight')
+#plt.axes.set_xlim([-10,10])
+#plt.axes.set_ylim([-10,10])
 plt.show()
 
+###############################################################################
+# Problem 5
+###############################################################################
+
+pos_labels = np.where(train_labels == 1)
+n_comp = 2
+pca_mod1 = PCA(n_components = n_comp)
+pca_dat1 = pca_mod1.fit_transform(train_data)
+pos_pca = pca_dat[pos_labels]
+model1 = GMM(n_components=4, covariance_type="full")
+model1.fit(pos_pca)
+
+neg_labels = np.where(train_labels == 0)
+n_comp = 2
+pca_mod2 = PCA(n_components = n_comp)
+pca_dat2 = pca_mod2.fit_transform(train_data)
+neg_pca = pca_dat2[neg_labels]
+model2 = GMM(n_components=4, covariance_type="full")
+model2.fit(neg_pca)
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from sklearn import mixture
+pca_mod3 = PCA(n_components = n_comp)
+pca_test_data = pca_mod3.fit_transform(test_data)
 
-n_samples = 300
+pos_score = model1.score(pca_test_data)
+neg_score = model2.score(pca_test_data)
 
-# generate random sample, two components
-np.random.seed(0)
 
-# generate spherical data centered on (20, 20)
-shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
+gmm_results = np.zeros([1124,])
+for i in range(pos_score.shape[0]):
+    if pos_score[i] > neg_score[i]:
+        gmm_results[i] = 1.0
+    elif pos_score[i] < neg_score[i]:
+        gmm_results[i] = 0.0
+    elif pos_score[i] == neg_score[i]:
+        print("50/50 situation faced")
+        gmm_results[i] = 1.0
 
-# generate zero centered stretched Gaussian data
-C = np.array([[0., -0.7], [3.5, .7]])
-stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
+metrics.accuracy_score(test_labels, gmm_results)
 
-# concatenate the two datasets into the final training set
-X_train = np.vstack([shifted_gaussian, stretched_gaussian])
+###############################################################################
+# Problem 6
+###############################################################################
 
-# fit a Gaussian Mixture Model with two components
-clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
-clf.fit(X_train)
 
-# display predicted scores by the model as a contour plot
-x = np.linspace(-20., 30.)
-y = np.linspace(-20., 40.)
-X, Y = np.meshgrid(x, y)
-XX = np.array([X.ravel(), Y.ravel()]).T
-Z = -clf.score_samples(XX)
-Z = Z.reshape(X.shape)
+# need different prams for pos and neg?
+pos_labels = np.where(train_labels == 1)
+n_comp = 2
+pca_mod1 = PCA(n_components = n_comp)
 
-CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0),
-                 levels=np.logspace(0, 3, 10))
-CB = plt.colorbar(CS, shrink=0.8, extend='both')
-plt.scatter(X_train[:, 0], X_train[:, 1], .8)
 
-plt.title('Negative log-likelihood predicted by a GMM')
-plt.axis('tight')
-plt.show()
+neg_labels = np.where(train_labels == 0)
+n_comp = 2
+pca_mod2 = PCA(n_components = n_comp)
+
 
 
 
